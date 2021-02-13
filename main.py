fbcefb52a2c3dfb2ce8ed07b6cfb8b51754260e2
Henrik Masbruch <henrik.masbruch@tum.de>
2021-02-13 22:28:47 +0100

    Plot with plotly

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 from sweep_line import sweep_line
 import pandas as pd
+import plotly.graph_objects as go
 
 
 def main(argv):
@@ -13,6 +14,7 @@ def main(argv):
                          "CollisionEnergy",
                          "Polarity"]
                      )
+    plotData(df)
 
     def handle_event_point(keys: set, accumulated_result: any):
         return max(accumulated_result, len(keys))
@@ -28,6 +30,41 @@ def main(argv):
 
     result = sweep_line(event_points(df), handle_event_point)
     print(result)
+
+
+def plotData(df: pd.DataFrame) -> None:
+    fig = go.Figure()
+    axis_layout = dict(
+            showline=True,
+            showgrid=False,
+            showticklabels=True,
+            linecolor='rgb(204, 204, 204)',
+            linewidth=2,
+            ticks='outside',
+            tickfont=dict(
+                family='Arial',
+                size=12,
+                color='rgb(82, 82, 82)',
+            ))
+    fig.update_layout(
+        plot_bgcolor="white",
+        yaxis_title="m/z",
+        xaxis_title="time (min)",
+        xaxis=axis_layout,
+        yaxis=axis_layout,
+    )
+    for index, row in df.iterrows():
+        fig.add_trace(
+            go.Scatter(x=[row["tStart"], row["tStop"]], y=[row["mOverZ"], row["mOverZ"]], mode="lines+markers", name=str(index)))
+
+    fig.show()
+
+
+def createAdder(y):
+    def add(y: int):
+        return lambda b: lambda a: a + y
+
+    return add
 
 
 if __name__ == '__main__':
